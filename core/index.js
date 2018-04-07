07ad015140baedecc6d2b1d1919747dcba7e7d27
Sergey D <dolgodvorov.sa@gmail.com>
2018-04-07 14:07:28 +0500

    Обновлена структура приложения; добавлены базовые стейты

--- core/index.js
+++ core/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("loaded");
-});
+const config = {
+  width: 800,
+  height: 600,
+  renderer: Phaser.AUTO,
+  antialias: false,
+  multiTexture: false
+}
+
+const game = new Phaser.Game(config);
+
+game.state.add('Boot', BootState);
+game.state.add('Game', GameState);
+game.state.add('GameOver', GameOverState);
+
+game.state.start('Boot');
---
+++ core/states/BootState.js
@@ -0,0 +1,39 @@
+class BootState extends Phaser.State {
+    create() {
+        this.title = new Phaser.Text(
+          this.game,
+          this.game.world.centerX,
+          this.game.world.centerY - 200,
+          'Erase\'Em Up',
+          {
+            font: '36px Tahoma',
+            fill: 'white',
+            align: 'center'
+          }
+        );
+
+        this.title.anchor.setTo(0.5);
+
+        this.start = new TextButton({
+            game: this.game,
+            x: this.game.world.centerX,
+            y: this.game.world.centerY,
+            overFrame: 2,
+            outFrame: 1,
+            downFrame: 0,
+            upFrame: 1,
+            label: 'Let\'s Rock',
+            style: {
+                font: '16px Verdana',
+                fill: 'white',
+                align: 'center'
+            }
+        });
+
+        this.start.onInputUp.add(() => this.state.start('Game'));
+
+        this.menuPanel = this.add.group();
+        this.menuPanel.add(this.title);
+        this.menuPanel.add(this.start);
+    }
+}
