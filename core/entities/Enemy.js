096a1cf9ec2d7c84b1bbf1102c049656f7c9d818
ArtemKhrulkov <artemhrulkov@gmail.com>
2018-04-07 17:37:06 +0500

    Добавлен класс врага и поменян GameState

---
+++ core/entities/Enemy.js
@@ -0,0 +1,54 @@
+// Missile constructor
+class Enemy extends Phaser.Sprite {
+    constructor(game, x, y, player) {
+        super(game, x, y, "rocket");
+        this.player = player; 
+
+        // Set the pivot point for this sprite to the center
+        this.anchor.setTo(0.5, 0.5);
+        // Enable physics on the enemy
+        game.physics.enable(this, Phaser.Physics.ARCADE);
+
+        // Define constants that affect motion
+        this.SPEED = 250; // enemy speed pixels/second
+        this.TURN_RATE = 5; // turn rate in degrees/frame
+    }
+
+
+    update() {
+        // Calculate the angle from the enemy to the mouse cursor game.input.x
+        // and game.input.y are the mouse position; substitute with whatever
+        // target coordinates you need.
+        var targetAngle = this.game.math.angleBetween(
+            this.x, this.y,
+            this.player.x, this.player.y
+        );
+
+        // Gradually (this.TURN_RATE) aim the enemy towards the target angle
+        if (this.rotation !== targetAngle) {
+            // Calculate difference between the current angle and targetAngle
+            var delta = targetAngle - this.rotation;
+
+            // Keep it in range from -180 to 180 to make the most efficient turns.
+            if (delta > Math.PI) delta -= Math.PI * 2;
+            if (delta < -Math.PI) delta += Math.PI * 2;
+
+            if (delta > 0) {
+                // Turn clockwise
+                this.angle += this.TURN_RATE;
+            } else {
+                // Turn counter-clockwise
+                this.angle -= this.TURN_RATE;
+            }
+
+            // Just set angle to target angle if they are close
+            if (Math.abs(delta) < this.game.math.degToRad(this.TURN_RATE)) {
+                this.rotation = targetAngle;
+            }
+        }
+
+        // Calculate velocity vector based on this.rotation and this.SPEED
+        this.body.velocity.x = Math.cos(this.rotation) * this.SPEED;
+        this.body.velocity.y = Math.sin(this.rotation) * this.SPEED;
+    }
+}
--- core/states/GameState.js
+++ core/states/GameState.js
@@ -1,17 +1,33 @@
 class GameState extends Phaser.State {
+  constructor() {
+    super();
+    this.MAX_ENEMIES = 6
+  }
+
   preload () {
     this.load.image('player', '/assets/sprites/player.png');
     this.load.image('player-cursor', '/assets/sprites/cursor.png');
+    this.load.image('rocket', '/assets/gfx/rocket.png');
+
+    this.game.load.spritesheet('explosion', '/assets/gfx/explosion.png', 128, 128);
   }
 
   create() {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+     // Create a group to hold the enemies
+     this.enemiesGroup = this.game.add.group();
+
+     // Create a group for explosions
+     this.explosionGroup = this.game.add.group();
+
     this.createPlayer();
+    this.createEnemies();
   }
 
   update() {
     this.updatePlayer();
+    this.updateEnemies();
   }
 
   render() {
@@ -27,6 +43,26 @@ class GameState extends Phaser.State {
     this.player.anchor.setTo(0.5, 0.5);
     this.game.physics.arcade.enable(this.player);
     this.player.body.collideWorldBounds = true;
+  }
+
+  createEnemies(x, y) {
+      let enemy = this.enemiesGroup.getFirstDead();
+
+      // If there aren't any available, create a new one
+      if (enemy === null) {
+          enemy = new Enemy(this.game,x,y,this.player);
+          this.enemiesGroup.add(enemy);
+      }
+  
+      // Revive the missile (set it's alive property to true)
+      // You can also define a onRevived event handler in your explosion objects
+      // to do stuff when they are revived.
+      enemy.revive();
+  
+      return enemy;
+      // this.game.add.existing(
+      //   new Enemy(this.game, this.game.width/2, this.game.height - 16, this.player)
+      // );
   }
 
   updatePlayer() {
@@ -57,5 +93,22 @@ class GameState extends Phaser.State {
     if (keyboard.isDown(Phaser.Keyboard.RIGHT)) {
       this.player.angle += ROTATE_STEP;
     }
+  }
+
+  updateEnemies() {
+    if (this.enemiesGroup.countLiving() < this.MAX_ENEMIES) {
+      // Set the launch point to a random location
+      this.createEnemies(this.game.rnd.integerInRange(50, this.game.width-50),
+          this.game.height + 50);
+    }
+
+    this.enemiesGroup.forEachAlive((m) => {
+        let distance = this.game.math.distance(m.x, m.y,
+            this.player.x, this.player.y);
+        if (distance < 50) {
+            m.kill();
+            // this.getExplosion(m.x, m.y);
+        }
+    });
   }
 }
